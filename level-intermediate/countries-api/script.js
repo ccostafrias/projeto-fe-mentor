b6b64c8226811110b3a6f3afcf99f68430b86969
Caique Costa <ccostafrias@gmail.com>
2023-02-13 22:29:39 -0300

    Atualizando "countries-api"

--- level-intermediate/countries-api/script.js
+++ level-intermediate/countries-api/script.js
@@ -1,15 +1,18 @@
 const enpoint = 'data.json'
 const countries = []
-const main = document.getElementsByTagName('main')[0]
+const cardsTable = document.querySelector('.cards-table')
+const cardTemplate = document.querySelector('.card, .template')
+const selectRegions = document.getElementById('regions')
+const searchInput = document.getElementById('search')
 
 fetch(enpoint)
-    .then(blob => blob.json())
-    .then(data => countries.push(...data))
+  .then(blob => blob.json())
+  .then(data => countries.push(...data))
 
 const observer = new PerformanceObserver((list) => {
   for (const entry of list.getEntries()) {
     if (entry.initiatorType === "fetch") {
-      startCountries()
+      startCountries(countries)
     }
   }
 });
@@ -18,10 +21,40 @@ observer.observe({
   entryTypes: ["resource"]
 });
 
-function startCountries(){
-    countries.forEach(country => {
-        var newCountry = document.createElement('div')
-        newCountry.textContent = 'Salve salve'
-        main.appendChild(newCountry)
-    });
+function startCountries(countriesTo) {
+  cardsTable.innerHTML = ''
+  const sortCountries = function (p = 'population', lm = 1) {
+    return countriesTo.sort((fcountry, scountry) => {
+      if (fcountry[p] > scountry[p]) {
+        return -1*lm
+      } else {
+        return 1*lm
+      }
+    })
+  }
+  sortCountries().forEach(country => {
+    let newCard = cardTemplate.cloneNode(true)
+    newCard.querySelector('.flag').src = country.flags.png
+    newCard.querySelector('h2').textContent = country.name
+    newCard.querySelectorAll('.subject')[0].textContent = country.population.toLocaleString()
+    newCard.querySelectorAll('.subject')[1].textContent = country.region
+    newCard.querySelectorAll('.subject')[2].textContent = country.capital
+    newCard.classList.remove('template')
+    cardsTable.appendChild(newCard)
+  });
 }
+
+selectRegions.addEventListener('input', () => {
+  if (selectRegions.value == '') {
+    startCountries(countries)
+  } else {
+    const filterRegion = countries.filter(country => country.region == selectRegions.options[selectRegions.selectedIndex].value)
+    startCountries(filterRegion)
+  }
+})
+
+searchInput.addEventListener('input', () => {
+  const filterSearch = countries.filter(country => String(country.name).toLocaleLowerCase().includes(String(searchInput.value).toLocaleLowerCase()))
+  startCountries(filterSearch)
+})
+
