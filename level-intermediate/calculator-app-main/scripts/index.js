495995fdefa495393f8a783270991d20ebac629e
Caique Costa <ccostafrias@gmail.com>
2023-04-02 01:30:29 -0300

    update operations and modifiers

--- level-intermediate/calculator-app-main/scripts/index.js
+++ level-intermediate/calculator-app-main/scripts/index.js
@@ -1,8 +1,12 @@
 const calcButtons = [...document.querySelectorAll('.calc-button')]
 const calcDisplay = document.querySelector('.calc-display-content')
+const calcDisplayOperations = document.querySelector('.calc-display-math')
 let isFirstPart = true
+let isFinish = false
 let hasComma = false
-let numA = numB = ''
+let numA = 0
+let numB = null
+let operator = null
 let result
 
 calcButtons.forEach(calcBttn => {
@@ -18,52 +22,131 @@ function handleButtonClick(e) {
         writeNum(num)
     } else if (type === 'del') {
         del()
+    } else if (type === 'operation') {
+        const signal = button.textContent
+        operate(signal)
+    } else if (type === 'reset') {
+        reset()
+    } else if (type === 'equals') {
+        calculate(numA, operator, numB)
+    } else if (type === 'modificator') {
+        const modificator = button.dataset.mod
+        modify(modificator)
     }
 }
 
 function writeNum(n) {
+    if (isFinish) {
+        isFinish = false
+        reset()
+    }
+
     if (calcDisplay.innerHTML == '0' && n != '.') calcDisplay.innerHTML = ''
 
     if (n == '.') {
         if (!hasComma) {
             calcDisplay.innerHTML += '.'
-            hasComma = !hasComma
+            hasComma = true
         }
     } else {
         calcDisplay.innerHTML += n
     }
 
-    if (isFirstPart) {
-        numA = calcDisplay.innerHTML
-    } else {
-        numB = calcDisplay.innerHTML
-    }
+    saveNums()
 }
 
 function del() {
     calcDisplay.innerHTML = '0'
     hasComma = false
 
+    saveNums()
+}
+
+function reset() {
+    calcDisplay.innerHTML = '0'
+    calcDisplayOperations.innerHTML = ''
+    hasComma = false
+    isFirstPart = true
+    numA = 0
+    numB = null
+}
+
+function saveNums() {
     if (isFirstPart) { 
-        numA = 0
+        numA = Number(calcDisplay.innerHTML)
     } else {
-        numB = 0
+        numB = Number(calcDisplay.innerHTML)
     }
 }
 
-function calculate(a, operator, b) {
-    switch (operator) {
-        case '+':
-            return a + b
+function operate(signal) {
+    if (isFinish) {
+        isFinish = false
+        isFirstPart = true
+    }
+    if (isFirstPart) {
+        operator = signal
+        hasComma = false
+        isFirstPart = false
+        numB = '0'
+        calcDisplayOperations.innerHTML = `${numA} ${operator}`
+        calcDisplay.innerHTML = '0'
+    } else {
+        calculate(numA, operator, numB)
+    }
+}
+
+function modify(modificator, result) {
+    const num = Number(calcDisplay.innerHTML)
+    switch (modificator) {
+        case '!':
+            if (num >= 0) {
+                let modified = num + 1
+                for (let i = 1; i < num + 1; i++) {
+                    modified *= i        
+                }
+                result = modified / (num + 1)
+            } else {
+                result = NaN
+            }
             break
-        case '-':
-            return a - b
+        case '%':
+            result = num / 100
+            if (result % Math.floor(result) !== 0) hasComma = true
             break
-        case 'x':
-            return a * b
+        case '^':
+            result = num ** 2
             break
-        case '/':
-            return a / b
+        case '±':
+            result = 0 - num
             break
     }
+
+    calcDisplay.innerHTML = result
+    saveNums()
+}
+
+function calculate(a, operator, b, result) {
+    if (!b && b !== 0) return
+    a = Number(a)
+    b = Number(b)
+    switch (operator) {
+        case '+':
+            result = a + b
+            break
+        case '-':
+            result = a - b
+            break
+        case 'x':
+            result = a * b
+            break
+        case '/':
+            result = a / b
+            break
+    }
+
+    calcDisplay.innerHTML = result
+    calcDisplayOperations.innerHTML = `${numA} ${operator} ${numB} =`
+    isFinish = true
+    numA = result
 }
